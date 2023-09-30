90a90f1e63f75cf3db69ecee0648032792298a8e
Djinadou sahid <djinadousahid@gmail.com>
2023-09-30 19:52:04 +0200

    Revert "Connexion d'un utilisateur"
    This reverts commit d492015aa98ea347a3462e13bd99373e31a17adc.

--- nom_du_projet/lib/main.dart
+++ nom_du_projet/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Connexionbod'un utilisateur"),
+          title: const Text("Connexion"),
         ),
         body:  Center(
             child: Column(
