d492015aa98ea347a3462e13bd99373e31a17adc
Djinadou sahid <djinadousahid@gmail.com>
2023-09-30 19:01:50 +0200

    Connexion d'un utilisateur

--- nom_du_projet/lib/main.dart
+++ nom_du_projet/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Connexion"),
+          title: const Text("Connexion d'un utilisateur"),
         ),
         body:  Center(
             child: Column(
